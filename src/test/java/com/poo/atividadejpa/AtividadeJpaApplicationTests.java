14d113be1ae9c4a296bbeb44b5e133d3f06a58d2
Felipe Pilz <felipevilvertpilz@gmail.com>
2025-09-21 13:10:04 -0300

    Initial commit

---
+++ src/test/java/com/poo/atividadejpa/AtividadeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.atividadejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
